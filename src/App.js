cf3f15e2483eb9b22149c3d32ce3792fba4b2d39
Emily Gaska <emilygaska@gmail.com>
2020-06-26 21:09:58 -0500

    Changed the build style and updated styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Profile from "./component/profile/profile";
 import Contact from "./component/contact/contact";
 import Portfolio from "./component/portfolio/portfolio";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
